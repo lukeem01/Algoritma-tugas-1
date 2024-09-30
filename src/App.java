9bad92e09832da4f089ba5385f2c82770316525a
Lukeem <eluqman012@gmail.com>
2024-09-30 11:43:39 +0700

    commit

---
+++ src/App.java
@@ -0,0 +1,15 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+         String nama = "Luqmanul Hakim";
+        // Double panjang = 20.0;
+        // Double lebar = 10.0;
+        // Double luas = panjang*lebar;
+        
+        int tahunLahir, tahunSekarang, umur;
+        tahunLahir = 2004;
+        tahunSekarang = 2024;
+        umur = tahunSekarang - tahunLahir;
+        System.out.println("Hello, "+nama+"");
+        System.out.println("Umur kamu adalah "+umur+" Tahun");
+    }
+}
---
+++ src/lukeem.java
@@ -0,0 +1,14 @@
+public class lukeem {
+    
+    public static void main(String[]argsStrings) throws Exception {
+        Double jumlah_KG_yang_dibeli_ = 7.0;
+        Double Total_yang_harus_dibayar = 168000.0;
+        Double Uang_yang_dibayar = 200000.0;
+        Double Uang_kembalian = Uang_yang_dibayar - Total_yang_harus_dibayar;
+    System.out.println("Ini bapak akan membeli "+jumlah_KG_yang_dibeli_+" jeruk");
+    System.err.println("Ini harga totalnya = "+Total_yang_harus_dibayar+"");
+    System.err.println("Baik pak uang bapak senilai = "+Uang_yang_dibayar+"");
+    System.out.println("dan ini uang kembalian "+Uang_kembalian+" nya ya pak");
+    System.err.println("Terima Kasih");
+    }
+}
