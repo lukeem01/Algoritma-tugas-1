f338b3a843a2c8dcea8dffcae5a35aaaaa8b817f
Lukeem <eluqman012@gmail.com>
2024-10-21 01:18:45 +0700

    message

--- src/Tugas_Sesi_4/Loop6.java
+++ src/Tugas_Sesi_4/Loop6.java
@@ -2,19 +2,22 @@ package Tugas_Sesi_4;
 
 public class Loop6 {
     public static void main(String[] args) {
-        int n = 9;
-        
-        int[] sequence = new int[n];
-        
-        sequence[0] = 1;
-        sequence[1] = 1;
-        
-        for (int i = 2; i < n; i++) {
-            sequence[i] = sequence[i-1] + sequence[i-2] + (i-2);
-        }
-        
-        for (int i = 0; i < n; i++) {
-            System.out.print(sequence[i] + " ");
+        int a = 0;
+        int b = 0;
+        int c = 0;
+        int d = 1;
+        int i = 2;
+        while (d <= 81) {
+            if (i == 2) {
+                i++;
+            } else {
+                System.out.print(d + " ");
+                d = a + b + c;
+
+            }
+            a = b;
+            b = c;
+            c = d;
         }
     }
 }
